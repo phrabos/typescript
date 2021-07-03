e7ff5343723fd0f1ba15d9f8f78fae3d2a2cc25f
patrick hrabos <phrabos@gmail.com>
2021-07-03 10:12:43 -0400

    interface

---
+++ src/Todos.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+const logTodo = (id: number, completed: string, title: boolean) => {
+	console.log(`
+  ${id} id
+  ${title} title
+  ${completed} completed
+  `);
+};
+interface Todo {
+	id: number;
+	title: string;
+	completed: boolean;
+}
+const Todos = () => {
+	useEffect(() => {
+		axios.get(url).then((res) => {
+			const todo = res.data as Todo;
+			const id = todo.id;
+			const title = todo.title;
+			const completed = todo.completed;
+
+			logTodo(id, title, completed);
+		});
+	}, []);
+
+	return (
+		<ul>
+			<li>todo</li>
+		</ul>
+	);
+};
+
+export default Todos;
