2095d46038e938b8a131fc0c9ab452a7f882fecb
patrick hrabos <phrabos@gmail.com>
2021-07-03 10:43:03 -0400

    types

--- src/types.ts
+++ src/types.ts
@@ -3,14 +3,14 @@ export const person = {
 	age: 20,
 };
 class Dog {
-  name;
-  age;
-  breed;
-  constructor(name: string, age: number, breed: string){
-    this.name = name,
-    this.age = age,
-    this.breed = breed,
-  }
+	name;
+	age;
+	breed;
+	constructor(name: string, age: number, breed: string) {
+		this.name = name;
+		this.age = age;
+		this.breed = breed;
+	}
 }
 
-const red = new Color();
+const spot = new Dog('spot', 5, 'poodle');
