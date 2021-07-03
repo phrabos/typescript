ed8d30eaf16ab07df7eb7129e896ccdfa530935d
patrick hrabos <phrabos@gmail.com>
2021-07-03 11:45:11 -0400

    functions and objects

---
+++ src/annotations/functions.ts
@@ -0,0 +1,47 @@
+/* eslint-disable */
+export const add = (a: number, b: number): number => {
+	return a + b;
+};
+
+export const subtract = (a: number, b: number): number => {
+	return a - b;
+};
+
+function divide(a: number, b: number): number {
+	return a / b;
+}
+
+const multiply = function (a: number, b: number): number {
+	return a * b;
+};
+
+const logger = (message: string): void => {
+	console.log(message);
+};
+
+const throwError = (message: string): never => {
+	throw new Error(message);
+};
+
+const throwError2 = (message: string): string => {
+	if (message === 'error') {
+		throw new Error('error');
+	}
+	return 'success';
+};
+
+const forecast = {
+	date: new Date(),
+	weather: 'sunny',
+};
+
+const logWeather = ({
+	date,
+	weather,
+}: {
+	date: Date;
+	weather: string;
+}): void => {
+	console.log(date);
+	console.log(weather);
+};
---
+++ src/annotations/objects.ts
@@ -0,0 +1,18 @@
+/* eslint-disable */
+export const profile = {
+	name: 'Alex',
+	age: 20,
+	coords: {
+		lat: 0,
+		lng: 15,
+	},
+	setAge(age: number): void {
+		this.age = age;
+	},
+};
+
+const { name, age }: { name: string; age: number } = profile;
+
+const {
+	coords: { lat, lng },
+}: { coords: { lat: number; lng: number } } = profile;
